1034073efa85f405eb9b0dee6c442b3202512769
blastwacia <ciatotoappdana@gmail.com>
2024-11-23 15:17:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -86,5 +86,5 @@ def send_messages():
         'failed_numbers': failed_numbers
     })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
